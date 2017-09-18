cef7e76c7c44ff6eff46c528540877ef9b185c79
eaccmk <eaccmk@users.noreply.github.com>
2017-09-18 15:28:35 +0530

    Study comments
    /* Study comments: 
    For Java: You can pass variable number of arguments to a method by using a Java feature called: varargs (Variable arguments). 
    The syntax to declare such method is:
    public void MethodName(type ... variableName) { }
    Ellipsis (three dots) is part of the syntax. 
    You can loop through the variableName just like you do with an array and fetch the arguments. 
    */

--- challenges/simple-addition-varargs.java
+++ challenges/simple-addition-varargs.java
@@ -1,13 +1,38 @@
-//Learn concept of varargs in java
+/*probmel statement:
+You are given a class Solution and its main method in the editor. 
+Your task is to create the class Add and the required methods so that the code prints the sum of the numbers passed to the function add.
+
+Note: Your add method in the Add class must print the sum as given in the Sample Output
+
+Input Format
+There are six lines of input, each containing an integer.
+
+Output Format
+There will be only four lines of output. Each line contains the sum of the integers passed as the parameters to add in the main method.
+
+Sample Input
+1
+2
+3
+4
+5
+6
+Sample Output
+
+1+2=3
+1+2+3=6
+1+2+3+4+5=15
+1+2+3+4+5+6=21
+
+*/ 
+//Here starts the program
+/**
+*
 @author : Millan Kaul
 @date: 18 Sep, 2017
-/*
-For Java: You can pass variable number of arguments to a method by using a Java feature called: varargs (Variable arguments). 
-The syntax to declare such method is:
-public void MethodName(type ... variableName) { }
-Ellipsis (three dots) is part of the syntax. 
-You can loop through the variableName just like you do with an array and fetch the arguments. 
-*/
+*
+**/
+
 
 import java.io.*;
 import java.lang.reflect.*;
